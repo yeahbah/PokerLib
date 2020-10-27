aa15c443e2b8f76207a20484e8769a8bb38f0feb
yeahbah <yeahbah@outlook.com>
2020-10-26 22:16:54 -0600

    Added sln file. Speed test project.

---
+++ SpeedTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Yeahbah.Poker;
+using Yeahbah.Poker.HandEvaluator;
+
+namespace SpeedTest
+{
+    static class Program
+    {
+        private static BlockingCollection<Card[]> _hands = new BlockingCollection<Card[]>();
+        private static int _numberOfHands = 10_000_000;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Setting up hands...");
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            SetupHands();
+            stopWatch.Stop();
+            Console.WriteLine($"Elapsed: {stopWatch.Elapsed}");
+            
+            Console.WriteLine($"Evaluating {_numberOfHands} hands...");
+            stopWatch.Start();
+            
+            var handsPerTask = _numberOfHands / 10;
+            var tasks = new List<Task>();
+            var hands = _hands.ToList();
+            for (int i = 0; i < 10; i++)
+            {
+                var h = hands.Take(handsPerTask);
+                var task = Task.Run(() =>
+                {
+                    foreach (var hand in h)
+                    {
+                        var evaluator = new DefaultHandEvaluator();
+                        var result = evaluator.Evaluate(hand);
+                    }
+                });
+                tasks.Add(task);
+                hands.RemoveRange(0, handsPerTask);
+            }
+
+
+            Task.WaitAll(tasks.ToArray());
+
+            stopWatch.Stop();
+            Console.WriteLine($"Elapsed = {stopWatch.Elapsed}");
+            var handsPerSecond = _numberOfHands / stopWatch.Elapsed.Seconds;
+            Console.WriteLine($"Hands per second = {handsPerSecond}");
+        }
+
+        private static void SetupHands()
+        {
+            var handsPerTask = _numberOfHands / 10;
+            var tasks = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                var task = Task.Run(() =>
+                {
+                    for (int j = 0; j < handsPerTask; j++)
+                    {
+                        var deck = new Deck();
+                        var hand = deck.TakeCards(5);
+                        _hands.Add(hand);       
+                    }
+                });    
+                tasks.Add(task);
+            }
+            
+            Task.WaitAll(tasks.ToArray());
+            _hands.CompleteAdding();
+        }
+    }
+}
